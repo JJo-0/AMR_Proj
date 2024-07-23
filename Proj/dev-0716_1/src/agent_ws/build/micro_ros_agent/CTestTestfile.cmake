d717e6ea84ea0d8e25f4b1d7b01a11edd28dc9be
Jiho <76779256+JJo-0@users.noreply.github.com>
2024-07-24 06:21:21 +0900

    Delete Proj/dev-0716_1 directory

--- Proj/dev-0716_1/src/agent_ws/build/micro_ros_agent/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gunwon/dev/src/agent_ws/src/uros/micro-ROS-Agent/micro_ros_agent
-# Build directory: /home/gunwon/dev/src/agent_ws/build/micro_ros_agent
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
